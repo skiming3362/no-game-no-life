abaf067b754c26d71f6d888d1171b9488810a58f
skiming <skiming@126.com>
2018-03-28 00:00:40 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'no-game-no-life'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
